d9a1d32d317f9e7159cde751b476ea1a44b9e8ec
amrelskhawy <68334383+amrelskhawy@users.noreply.github.com>
2022-04-10 05:41:46 +0200

    after swiper

--- js/main.js
+++ js/main.js
@@ -1,48 +1,61 @@
 /*=============== SHOW MENU ===============*/
-const   show = document.getElementById('nav-toggle'),
-        close = document.getElementById('nav-close'),
-        menu = document.querySelector('.nav__menu'),
-        menu_items = document.querySelectorAll('.nav__item')
+const show = document.getElementById("nav-toggle"),
+    close = document.getElementById("nav-close"),
+    menu = document.querySelector(".nav__menu"),
+    menu_items = document.querySelectorAll(".nav__item");
 
-show.addEventListener("click", ()=> {
-    menu.classList.add('show-menu')
-})
+show.addEventListener("click", () => {
+    menu.classList.add("show-menu");
+});
 
-close.addEventListener("click", ()=> {
-    menu.classList.remove('show-menu')
-})
+close.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+});
 
-menu_items.forEach(link => {
-    link.addEventListener("click", ()=> {
-        menu.classList.remove('show-menu')
-    }
-    )
-})
+menu_items.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show-menu");
+    });
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const header = document.getElementById('header')
+const header = document.getElementById("header");
 function scrollHeader() {
     if (this.scrollY >= 50) {
-        header.classList.add('scroll-header');
+        header.classList.add("scroll-header");
+    } else {
+        header.classList.remove("scroll-header");
     }
-    else{
-        header.classList.remove('scroll-header');
-    } 
 }
-window.addEventListener("scroll" , scrollHeader)
-/*=============== POPULAR SWIPER ===============*/
+window.addEventListener("scroll", scrollHeader);
 
+
+/*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            spaceBetween: 48,
+        },
+      },
+  });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
 
+/* Link active featured */
 
-/* Link active featured */ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
