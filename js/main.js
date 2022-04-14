1608a3f817df2cd45db182eefb47ed1a36f08eba
amrelskhawy <68334383+amrelskhawy@users.noreply.github.com>
2022-04-14 02:58:36 +0200

    final

--- js/main.js
+++ js/main.js
@@ -91,5 +91,26 @@ scrollUp.addEventListener('click', (e)=> {
 })
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = function() {
+    const scrollY = window.pageYOffset
+    
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58 ,
+            sectionId = current.getAttribute('id')
+        
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList
+                .add('active-link')
+            } else {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList
+                .remove('active-link')
+            }
+    })
+}
+window.addEventListener("scroll", scrollActive);
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
